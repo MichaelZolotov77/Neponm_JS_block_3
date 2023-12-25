26bd96a841b8e582f365b91cdf1fc7e8f05d6e5f
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-25 21:05:43 +0300

    Методы объекта и его прототипа

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const developer = {
   age: 30,
   skills: ["JavaScript", "TypeScript", "CSS"],
   isMaried: false,
+  addAge() {
+    this.age++;
+    console.log(this);
+  },
+  getMarried() {
+    this.isMaried = true;
+  },
+  getDivorced() {
+    this.isMaried = false;
+  },
 };
 
-// for (let prop in developer) {
-//   console.log(prop, developer[prop]); // перебор ключей и значений
-// }
-
-const numbers = [1, 2, 3, 4, 5];
-
-for (let number of numbers) {
-  console.log(number);
-}
+console.log(developer.hasOwnProperty("name"));

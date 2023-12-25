3276e13c5599ad206ecfba9b4f7432e8ce1bcebd
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-25 20:41:05 +0300

    Перебор ключей и значений в цикле for in

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const developer = {
   name: "Vasiliy",
   surname: "Popov",
+  age: 30,
+  skills: ["JavaScript", "TypeScript", "CSS"],
   isMaried: false,
 };
-console.log("surname" in developer); // проверка наличия ключа с заданным именем в объекте
+
+for (let prop in developer) {
+  console.log(prop, developer[prop]); // перебор ключей и значений
+}

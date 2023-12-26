566c4adf72fce4656e25ab6e8852571411cd14a1
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-26 15:12:45 +0300

    Некоторые методы строк

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const person = {
-  name: "Anna",
-  surname: "Dance",
-  age: 18,
-  greeting() {
-    console.log("hello");
-  },
-  changeSurname(surname) {
-    this.surname = surname;
-  },
-};
+const str = "Hello World";
 
-person.greeting();
-person.changeSurname("Musatova");
-console.log(person);
+console.log(str.toUpperCase()); // исходная строка не меняется
+console.log(str.toLowerCase());
+
+console.log(str.includes("World")); // есть ли в строке
+console.log(str.indexOf("World")); // 6 (индекс если есть, -1 если нет)
+
+console.log(str.slice(0, 5)); // нарезать строку, с 0 по 5 индекс не включая
+console.log(str.slice(6)); // с 6 индекса и до конца строки
+
+console.log(str.split(" ")); // создать массив по разделителю пробел
+// chaining, когда методы идут один за другим
+// создать опять из массива строку и поставить между ячейками разделитель
+console.log(str.split(" ").join(" | "));

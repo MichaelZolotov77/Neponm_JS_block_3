b278e6008838975a36f1e774c143d08ca6dc49f3
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-26 21:35:43 +0300

    Угадай загадку. Вариант преподавателя

--- main.js
+++ main.js
@@ -3,18 +3,27 @@ var riddle = {
   correctAnswer: "лампочка",
   hints: ["это несъедобное", "это не фрукт"],
   tries: 3,
-  checkAnswer(guessedAnswer, hint) {
+  checkAnswer(str) {
     // TODO: написать логику проверки правильного ответа
     // alert для пользователя, console.log()
-
-    if (guessedAnswer.toLowerCase() === this.correctAnswer) {
+    if (this.tries < 1) {
+      console.log("Игра окончена");
+      return alert("Игра окончена");
+    }
+    if (str.toLowerCase().includes(this.correctAnswer)) {
       alert("Правильный ответ");
       console.log("Правильный ответ");
       this.tries = 0;
     } else {
-      alert(`Неправильный ответ. Посказка: ${this.hints[hint]}`);
+      alert("Неправильный ответ");
       console.log("Неправильный ответ");
       this.tries--;
+
+      const hint = this.hints[this.tries === 2 ? 0 : 1];
+
+      if (this.tries) {
+        alert("Подсказка: " + hint);
+      }
     }
   },
 };
@@ -23,15 +32,12 @@ window.onload = function () {
   document.getElementById("riddle").innerText = riddle.question;
 };
 
-let hint = 0;
 function check() {
   var input = document.getElementsByTagName("input")[0];
 
   var guessedAnswer = input.value;
-  console.log(guessedAnswer);
-  if (guessedAnswer && riddle.tries > 0) {
+  if (guessedAnswer) {
     // TODO: вызвать функцию checkAnswer у объекта загадки, передав туда ответ
-    riddle.checkAnswer(guessedAnswer, hint);
-    hint++;
+    riddle.checkAnswer(guessedAnswer);
   }
 }

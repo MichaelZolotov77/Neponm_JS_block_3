eb698f4891973f5e8c03d84fa5793c6594eb145d
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-26 15:39:22 +0300

    Пара методов чисел

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const str = "Hello World";
+const num = 1180000.15555;
 
-console.log(str.toUpperCase()); // исходная строка не меняется
-console.log(str.toLowerCase());
+console.log(num.toFixed(2));
 
-console.log(str.includes("World")); // есть ли в строке
-console.log(str.indexOf("World")); // 6 (индекс если есть, -1 если нет)
-
-console.log(str.slice(0, 5)); // нарезать строку, с 0 по 5 индекс не включая
-console.log(str.slice(6)); // с 6 индекса и до конца строки
-
-console.log(str.split(" ")); // создать массив по разделителю пробел
-// chaining, когда методы идут один за другим
-// создать опять из массива строку и поставить между ячейками разделитель
-console.log(str.split(" ").join(" | "));
+console.log(
+  num.toLocaleString("ru-RU", { style: "currency", currency: "RUB" })
+);

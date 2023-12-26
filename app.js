2c2939ea74ab56d5e0f1341954b46fa538e495bd
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-26 17:04:09 +0300

    Задание 3.10

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const numbers = [1, 2, 3, 4];
+// 1
+const fruits = ["apple", "banana", "melon", "orange"];
+const hasOrange = fruits.includes("orange");
+console.log(hasOrange);
+// 2
+const cars = ["BMW", "Nissan", "VW", "Skoda", "Audi", "Kia"];
+const favoriteCars = cars.slice(0, 3);
+console.log(favoriteCars);
 
-// push, pop - добавить и удалить в конце массива
-// shift, unshift - добавить и удалить в начале массива
-
-// есть ли в массиве такое значение
-console.log(numbers.includes(4));
-// какой индекс в массиве, если не найдет то -1
-console.log(numbers.indexOf(2));
-// получить новый массив по срезу
-console.log(numbers.slice(0, 2));
-// в массив добавить еще ячеек, предыдущий остается без изменений
-const nums2 = numbers.concat([6, 7, 8]);
-console.log(nums2);
-// создать из массива строку, между ячейками добавить запятую и пробел
-console.log(numbers.join(", "));
+// 3
+const otherCars = cars.slice(-3);
+console.log(otherCars);

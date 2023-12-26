db6b411893b160652af6148d1dfb39d226bfa221
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-26 16:54:47 +0300

    Некоторые методы массивов

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const num = 1180000.15555;
+const numbers = [1, 2, 3, 4];
 
-console.log(num.toFixed(2));
+// push, pop - добавить и удалить в конце массива
+// shift, unshift - добавить и удалить в начале массива
 
-console.log(
-  num.toLocaleString("ru-RU", { style: "currency", currency: "RUB" })
-);
+// есть ли в массиве такое значение
+console.log(numbers.includes(4));
+// какой индекс в массиве, если не найдет то -1
+console.log(numbers.indexOf(2));
+// получить новый массив по срезу
+console.log(numbers.slice(0, 2));
+// в массив добавить еще ячеек, предыдущий остается без изменений
+const nums2 = numbers.concat([6, 7, 8]);
+console.log(nums2);
+// создать из массива строку, между ячейками добавить запятую и пробел
+console.log(numbers.join(", "));
